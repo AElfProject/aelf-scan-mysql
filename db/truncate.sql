f91bc676c626420a92bc66f9d279dda7d0ab74bf
atom-young <atom.yang@aelf.io>
2019-08-22 15:07:52 +0800

    feat: 🎸 add truncate sql script;

---
+++ db/truncate.sql
@@ -0,0 +1,17 @@
+use aelf_main_chain;
+
+TRUNCATE TABLE contract_aelf20;
+
+TRUNCATE TABLE transactions_0;
+
+TRUNCATE TABLE transactions_unconfirmed;
+
+TRUNCATE TABLE resource_0;
+
+TRUNCATE TABLE resource_unconfirmed;
+
+TRUNCATE TABLE tps_0;
+
+TRUNCATE TABLE blocks_0;
+
+TRUNCATE TABLE blocks_unconfirmed;
